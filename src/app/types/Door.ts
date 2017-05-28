a0bf5d1f70813d21b94fe36f9d1ec1d4d5aacaf0
Freebit <freebit77@gmail.com>
2017-05-28 17:14:17 +0300

    Додабвены основные типы

---
+++ src/app/types/Door.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by freebit on 28/05/2017.
+ */
+export class Door{
+    title: string;
+    imgSrc: string;
+}
---
+++ src/app/types/Interior.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by freebit on 28/05/2017.
+ */
+export class Interior{
+    title: string;
+    imgSrc: string;
+}
